9869ab2c9462f985848b189a936dd04513464316
kkavinkk <gk.kavin05@gmail.com>
2025-03-12 12:59:29 -0600

    working on header

--- Kavins Website/src/components/header.tsx
+++ Kavins Website/src/components/header.tsx
@@ -1,5 +1,15 @@
 import { Container } from "./styles";
 import { BrowserRouter as Router } from "react-router-dom"; // keep UI in sinc with URL
 import { NavHaskLink, HaskLink } from "react-router-hash-link";
-import { useState } from "react";
+import { useState } from "react"; // Track data or properties from this function
 // import something like resume from assets
+
+export function Header() {}
+const [isActive, setActive] = useState(false);
+function toggleTheme() {
+  let html = document.getElementsByTagName("html")[0];
+  html.classList.toggle("light");
+}
+function closeMenu() {
+  setActive(false);
+}
