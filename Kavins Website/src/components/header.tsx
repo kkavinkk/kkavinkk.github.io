d437227727d227b0371cdfd3abb86b879bf27dc6
kkavinkk <gk.kavin05@gmail.com>
2025-03-12 12:53:56 -0600

    working on header

---
+++ Kavins Website/src/components/header.tsx
@@ -0,0 +1,5 @@
+import { Container } from "./styles";
+import { BrowserRouter as Router } from "react-router-dom"; // keep UI in sinc with URL
+import { NavHaskLink, HaskLink } from "react-router-hash-link";
+import { useState } from "react";
+// import something like resume from assets
