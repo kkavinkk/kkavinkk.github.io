d14888009f087ac2cbc030c3d9f746e092f1d10d
kkavinkk <gk.kavin05@gmail.com>
2025-03-15 13:22:41 -0600

    header is coming along

--- Kavins Website/src/components/Header.tsx
+++ Kavins Website/src/components/Header.tsx
@@ -4,7 +4,17 @@ import styles from "./Header.module.css";
 const Header: React.FC = () => {
   return (
     <header className={styles.header}>
-      <div className="container mx-auto flex justify-between items-center">
+      <nav className="sm:px-16 w-full flex items-center py-5 fixed top-0 z-20 bg-transparent">
+        <div className="w-full flex justify-between items-center max-w-7xl mx-auto">
+          <a href="#" className="flex items-center gap-2">
+            <p className="text-white text-[25px] font-bold cursor-pointer tracking-tight">
+              Kavin.&nbsp;
+            </p>
+          </a>
+          {/* <ul className="list-none hidden"></ul> */}
+        </div>
+      </nav>
+      {/* <div className="container mx-auto flex justify-between items-center">
         <h1 className={styles.title}>Kavins Website</h1>
         <nav className={styles.nav}>
           <a href="#about">About</a>
@@ -12,7 +22,7 @@ const Header: React.FC = () => {
           <a href="#projects">Projects</a>
           <a href="#contact">Contact</a>
         </nav>
-      </div>
+      </div> */}
     </header>
   );
 };
