847e8c7e42f508ba5ced7086a651d18044886e1e
kkavinkk <gk.kavin05@gmail.com>
2025-03-15 14:15:57 -0600

    ts hard

--- Kavins Website/src/components/Header.tsx
+++ Kavins Website/src/components/Header.tsx
@@ -3,28 +3,26 @@ import styles from "./Header.module.css";
 
 const Header: React.FC = () => {
   return (
-    <header className={styles.header}>
-      <nav className="sm:px-16 w-full flex items-center py-5 fixed top-0 z-20 bg-transparent">
-        <div className="w-full flex justify-between items-center max-w-7xl mx-auto">
+    <header className="{Header-main}">
+      <nav className="{Full-header}">
+        <div className="header-text">
           <a href="#" className="flex items-center gap-2">
-            <p className="text-white text-[25px] font-bold cursor-pointer tracking-tight">
-              Kavin.&nbsp;
-            </p>
+            <p className="Name">Kavin.&nbsp;</p>
           </a>
-          <ul className="list-none hidden sm:flex flex-row gap-10 ml-auto mr-6">
-            <li className="text-secondary hover:text-white text-[18px] font-medium cursor-pointer">
+          <ul className="list-header">
+            <li className="list-component">
               <a href="#about">About</a>
             </li>
-            <li className="text-secondary hover:text-white text-[18px] font-medium cursor-pointer">
+            <li className="list-component">
               <a href="#experience">Experience</a>
             </li>
-            <li className="text-secondary hover:text-white text-[18px] font-medium cursor-pointer">
+            <li className="list-component">
               <a href="projects">Projects</a>
             </li>
-            <li className="text-secondary hover:text-white text-[18px] font-medium cursor-pointer">
+            <li className="list-component">
               <a href="gallery">Gallery</a>
             </li>
-            <li className="text-secondary hover:text-white text-[18px] font-medium cursor-pointer">
+            <li className="list-component">
               <a href="contact">Contact</a>
             </li>
           </ul>
