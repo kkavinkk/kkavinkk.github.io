055034327d82780124f9118e5f46b4cccafa2e04
kkavinkk <gk.kavin05@gmail.com>
2025-03-17 15:25:10 -0600

    epic packgorund video

--- Kavins Website/src/components/Header.tsx
+++ Kavins Website/src/components/Header.tsx
@@ -4,11 +4,11 @@ import "./Header.css";
 const Header: React.FC = () => {
   return (
     <header className="{Header-main}">
-      <nav className="sm:px-16 px-3 w-full  flex items-center py-5 fixed top-0 z-20 bg-transparent">
+      <nav className="sm:px-16 px-3 w-full  flex items-center py-5 fixed top-0 z-20 bg-[#ffffff]">
         <div className="w-full  flex  justify-between items-center max-w-7x1 mx-auto">
           <a href="#" className="flex items-center gap-2">
             <p className="text-black text-[25px] font-bold cursor-pointer flex logo reduce-letter-spacing">
-              Kavin.&nbsp;
+              Kavin.K&nbsp;
             </p>
           </a>
           <ul className="list-none hidden sm:flex flex-row gap-10 ml-auto mr-6">
