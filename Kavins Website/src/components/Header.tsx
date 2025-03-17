ca6d462728938365c33b882de589495df9470677
kkavinkk <gk.kavin05@gmail.com>
2025-03-17 10:47:17 -0600

    HADER STEAMMMM

--- Kavins Website/src/components/Header.tsx
+++ Kavins Website/src/components/Header.tsx
@@ -7,7 +7,9 @@ const Header: React.FC = () => {
       <nav className="sm:px-16 px-3 w-full  flex items-center py-5 fixed top-0 z-20 bg-transparent">
         <div className="w-full  flex  justify-between items-center max-w-7x1 mx-auto">
           <a href="#" className="flex items-center gap-2">
-            <p className="flex items-center gap-2">Kavin.&nbsp;</p>
+            <p className="text-black text-[25px] font-bold cursor-pointer flex logo reduce-letter-spacing">
+              Kavin.&nbsp;
+            </p>
           </a>
           <ul className="list-none hidden sm:flex flex-row gap-10 ml-auto mr-6">
             <li className="text-secondary hover:text-white text-[18px] font-medium cursor-pointer">
