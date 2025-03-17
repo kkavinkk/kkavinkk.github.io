7316b5110f0f83d6cbbc284d56b31b4d9f76b384
kkavinkk <gk.kavin05@gmail.com>
2025-03-17 11:48:12 -0600

    IM COOKING UM A STORM

--- Kavins Website/src/components/Face.tsx
+++ Kavins Website/src/components/Face.tsx
@@ -11,6 +11,10 @@ const Face: React.FC = () => {
         <h1 className="text-[#3559F4] font-medium lg:text-[30px] sm:text-[26px] xs:text-[20px] text-[16px] lg:leading-[40px">
           <p className="name">WAGWAN CRODIE!</p>
         </h1>
+        <h1 className="font-black text-white lg:texxt-[80px] sm:text-[60px] xs:text-[50px] text-[40px] lg:leading-[98px] mt-2 text-black">
+          <span className="text-[#529dff]">Kavin</span>
+          <span className="text-[#f43535]">Kumaran</span>
+        </h1>
       </div>
     </div>
   );
