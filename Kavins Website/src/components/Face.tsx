101e2fdd66378f8cdb6e1438f4cb2679cbb2ed4b
kkavinkk <gk.kavin05@gmail.com>
2025-03-18 10:49:35 -0600

    CAN WE GET %

--- Kavins Website/src/components/Face.tsx
+++ Kavins Website/src/components/Face.tsx
@@ -14,20 +14,18 @@ const Face: React.FC = () => {
     //   </video>
     <div className="absolute inset-0 top-[120px] max-w-7x1 mx-auto sm:px-16 px-3 flex flex-row items-start gap-5">
       <div className="flex flex-col justify-center items-center mt-5">
-        <div className="w-5 h-5 square-full bg-black"></div>
-        <div className="w-1 sm:h-86 h-40 bg-black"></div>
+        <div className="w-5 h-5 rounded-full bg-[#008080]"></div>
+        <div className="w-1 sm:h-86 h-40 bg-[#008080]"></div>
       </div>
       <div className="flex flex-col justify-center z-10 relative">
-        <h1 className="text-[#3559F4] font-serif lg:text-[30px] sm:text-[26px] xs:text-[20px] text-[16px] lg:leading-[40px">
+        <h1 className="text-[#ffffff] font-serif lg:text-[30px] sm:text-[26px] xs:text-[20px] text-[16px] lg:leading-[40px">
           <p className="name">👋Hi, My name is </p>
         </h1>
         <h1 className="font-black text-white lg:text-[80px] sm:text-[60px] xs:text-[50px] text-[40px] lg:leading-[98px] mt-2 text-black">
-          <span className="text-[#6acfff] font-sans drop-shadow-[0_0_10px_#529dff]">
-            Kavinvasan
-          </span>
+          <span className="text-[#fcbb53] font-sans">Kavinvasan</span>
           <span> </span>
           <span className="text-white font-sans drop-shadow-[0_0_10px_#529dff]">
-            KUMARAN
+            Kumaran
           </span>
         </h1>
         <p className="text-black font-mediem lg:text-[30px] sm:text-[26px] xs:text-[20px] text-[16px] lg:leading-[40px] mt-2 text-black-100 realtive z-10">
@@ -37,7 +35,8 @@ const Face: React.FC = () => {
           <br className="sm:block hidden" />
         </p>
         <br />
-        <div className="flex item-center gap-3 drop-shadow-[0_0_10px_#529dff]">
+
+        <div className="flex item-center gap-3">
           {/* GitHub */}
           <a
             href="https://github.com/kkavinkk"
@@ -46,17 +45,11 @@ const Face: React.FC = () => {
           >
             <svg
               xmlns="http://www.w3.org/2000/svg"
-              fill="currentColor"
+              fill="white"
               viewBox="0 0 24 24"
-              strokeWidth="1.5"
-              stroke="currentColor"
               className="w-10 h-10"
             >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M12 2.25c-5.385 0-9.75 4.365-9.75 9.75 0 4.31 2.797 7.97 6.675 9.26.488.09.665-.21.665-.47 0-.23-.009-.84-.014-1.65-2.716.59-3.29-1.31-3.29-1.31-.445-1.14-1.086-1.44-1.086-1.44-.89-.605.07-.59.07-.59 1.003.07 1.53 1.03 1.53 1.03.875 1.5 2.295 1.07 2.855.82.09-.635.34-1.07.615-1.315-2.17-.25-4.455-1.085-4.455-4.83 0-1.07.375-1.945 1.005-2.63-.1-.25-.435-1.27.1-2.64 0 0 .82-.26 2.69 1.005A9.26 9.26 0 0 1 12 6.84c.83.005 1.67.11 2.46.32 1.865-1.265 2.685-1.005 2.685-1.005.54 1.37.205 2.39.1 2.64.63.685 1.005 1.56 1.005 2.63 0 3.75-2.29 4.575-4.465 4.83.35.3.66.885.66 1.785 0 1.29-.01 2.33-.01 2.64 0 .265.175.565.67.47 3.87-1.29 6.665-4.95 6.665-9.26 0-5.385-4.365-9.75-9.75-9.75z"
-              />
+              <path d="M12 2.25c-5.385 0-9.75 4.365-9.75 9.75 0 4.31 2.797 7.97 6.675 9.26.488.09.665-.21.665-.47 0-.23-.009-.84-.014-1.65-2.716.59-3.29-1.31-3.29-1.31-.445-1.14-1.086-1.44-1.086-1.44-.89-.605.07-.59.07-.59 1.003.07 1.53 1.03 1.53 1.03.875 1.5 2.295 1.07 2.855.82.09-.635.34-1.07.615-1.315-2.17-.25-4.455-1.085-4.455-4.83 0-1.07.375-1.945 1.005-2.63-.1-.25-.435-1.27.1-2.64 0 0 .82-.26 2.69 1.005A9.26 9.26 0 0 1 12 6.84c.83.005 1.67.11 2.46.32 1.865-1.265 2.685-1.005 2.685-1.005.54 1.37.205 2.39.1 2.64.63.685 1.005 1.56 1.005 2.63 0 3.75-2.29 4.575-4.465 4.83.35.3.66.885.66 1.785 0 1.29-.01 2.33-.01 2.64 0 .265.175.565.67.47 3.87-1.29 6.665-4.95 6.665-9.26 0-5.385-4.365-9.75-9.75-9.75z" />
             </svg>
           </a>
 
@@ -65,11 +58,10 @@ const Face: React.FC = () => {
             href="https://www.linkedin.com/in/kavinvasan-kumaran-7a24352a1/"
             target="_blank"
             rel="noopener noreferrer"
-            className="text-[#0A66C2] hover:text-[#004182]"
           >
             <svg
               xmlns="http://www.w3.org/2000/svg"
-              fill="currentColor"
+              fill="white"
               viewBox="0 0 24 24"
               className="w-10 h-10"
             >
@@ -82,11 +74,10 @@ const Face: React.FC = () => {
             href="https://www.instagram.com/kavinn_kk/?hl=en"
             target="_blank"
             rel="noopener noreferrer"
-            className="text-[#E4405F] hover:text-[#C13584]"
           >
             <svg
               xmlns="http://www.w3.org/2000/svg"
-              fill="currentColor"
+              fill="white"
               viewBox="0 0 24 24"
               className="w-10 h-10"
             >
