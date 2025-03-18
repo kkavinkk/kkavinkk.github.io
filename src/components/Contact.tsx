41be82f35f7e33bd3d532a75163f284d737be26a
kkavinkk <gk.kavin05@gmail.com>
2025-03-18 14:19:31 -0600

    yes

--- src/components/Contact.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const contact: React.FC = () => {
-  return <h3>Contact</h3>;
-};
-
-export default contact;
--- vite.config.ts
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
-import tailwindcss from '@tailwindcss/vite'
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  base: 'kkavink.github.io', // 👈 Add this line (replace with your repo name)
-})
-
