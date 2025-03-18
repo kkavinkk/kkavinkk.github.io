5507004f7ce6963cfff6bb7a8148fa23c5c0bc43
kkavinkk <gk.kavin05@gmail.com>
2025-03-18 15:37:31 -0600

    stop

--- src/components/Experience.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const experience: React.FC = () => {
-  return <h3>Experience</h3>;
-};
-
-export default experience;
--- src/components/Projects.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const projects: React.FC = () => {
-  return <h3>Projects</h3>;
-};
-
-export default projects;
