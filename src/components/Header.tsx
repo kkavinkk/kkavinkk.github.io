8843d080683e7b0ff8afa39eee36e201764d1232
kkavinkk <gk.kavin05@gmail.com>
2025-03-18 14:35:25 -0600

    yes

---
+++ src/components/Header.tsx
@@ -0,0 +1,37 @@
+import React from "react";
+import "./Header.css";
+
+const Header: React.FC = () => {
+  return (
+    <header className="{Header-main}">
+      <nav className="sm:px-16 px-3 w-full  flex items-center py-5 fixed top-0 z-20 bg-transparent">
+        <div className="w-full  flex  justify-between items-center max-w-7x1 mx-auto">
+          <a href="#" className="flex items-center gap-2">
+            <p className="text-white text-[25px] font-bold cursor-pointer flex logo reduce-letter-spacing">
+              Kavin.K&nbsp;
+            </p>
+          </a>
+          <ul className="list-none hidden sm:flex flex-row gap-10 ml-auto mr-6">
+            <li className="text-white hover:text-white text-[18px] font-medium cursor-pointer">
+              <a href="#about">About</a>
+            </li>
+            <li className="text-white hover:text-white text-[18px] font-medium cursor-pointer">
+              <a href="#experience">Experience</a>
+            </li>
+            <li className="text-white hover:text-white text-[18px] font-medium cursor-pointer">
+              <a href="projects">Projects</a>
+            </li>
+            <li className="text-white hover:text-white text-[18px] font-medium cursor-pointer">
+              <a href="gallery">Gallery</a>
+            </li>
+            <li className="text-white hover:text-white text-[18px] font-medium cursor-pointer">
+              <a href="contact">Contact</a>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
---
+++ src/components/Projects.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const projects: React.FC = () => {
+  return <h3>Projects</h3>;
+};
+
+export default projects;
