f952bfbec001d2dc9080989c862a18a323a157f8
kkavinkk <gk.kavin05@gmail.com>
2025-03-18 14:13:54 -0600

    deploy pleaze

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  base: 'kkavink.github.io', // 👈 Add this line (replace with your repo name)
 })
 
