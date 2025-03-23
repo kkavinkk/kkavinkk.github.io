49a64f816389936fee9ef89bdd80c151e34b320b
kkavinkk <gk.kavin05@gmail.com>
2025-03-23 11:28:21 -0600

    doen projs

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -9,6 +9,7 @@ const Projects: React.FC = () => {
           Projects
         </h2>
       </div>
+      {/* Project #1: Fact or Cap */}
       <div className="project-container bg-black">
         <div className="p-contain-1">
           <div className="links-wrap space-x-4">
@@ -19,7 +20,7 @@ const Projects: React.FC = () => {
             >
               <img
                 className="w-80 h-auto object-cover rounded-lg border-4 border-white bg-white"
-                src="public\FactORCap.png"
+                src="\FactORCap.png"
                 alt="FactOrCap"
               />
             </a>
@@ -65,6 +66,65 @@ const Projects: React.FC = () => {
             </button>
             <button className="icon-house">
               <p>Google Fact Checking API</p>
+            </button>
+          </div>
+        </div>
+      </div>
+
+      {/* Project #2: Sensaid */}
+      <div className="project-container bg-black">
+        <div className="p-contain-1">
+          <div className="links-wrap space-x-4">
+            <a
+              href="https://github.com/talebirad/SenseAid"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <img
+                className="w-80 h-auto object-cover rounded-lg border-4 border-white bg-white"
+                src="\senseAid.png"
+                alt="FactOrCap"
+              />
+            </a>
+            <div className="text-center space-y-6 space-x-4">
+              <a href="https://github.com/talebirad/SenseAid" target="_blank">
+                <button className="bg-blue-500 hover:bg-blue-700 text-white font-sans py-2 px-4 rounded-full">
+                  GitHub
+                </button>
+              </a>
+              <a
+                href="https://devpost.com/software/senseaid-4wgt5b"
+                target="_blank"
+              >
+                <button className="bg-blue-500 hover:bg-blue-700 text-white font-sans py-2 px-4 rounded-full">
+                  DEVPOST
+                </button>
+              </a>
+            </div>
+          </div>
+        </div>
+        <div className="p-contain-2 text-white">
+          <h2 className="text-4xl font-extrabold dark:text-white">SenseAid</h2>
+          <p>
+            SenseAid is a cue-based sensory therapy software designed to treat
+            patients with sensory-related issues, particularly older adults. It
+            tracks the patient's hand and body movements in real-time, projects
+            them onto the patient's body, and provides visual and audio cues to
+            assist clinicians during sensory exercises.
+          </p>
+          <p>3rd place at natHacks 2024</p>
+          <div className="icon-wrap items-center">
+            <button className="icon-house">
+              <p>Python</p>
+            </button>
+            <button className="icon-house">
+              <p>MediaPipe</p>
+            </button>
+            <button className="icon-house">
+              <p>PyGame</p>
+            </button>
+            <button className="icon-house">
+              <p>NumPy</p>
             </button>
           </div>
         </div>
