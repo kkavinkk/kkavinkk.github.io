b47a98ddb300cc14bc3589dc2cd6062e2f555883
kkavinkk <gk.kavin05@gmail.com>
2025-03-22 19:07:54 -0600

    projects frame is done

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,38 +1,75 @@
 import React from "react";
+import "./projects.css";
 
 const Projects: React.FC = () => {
   return (
-    <div className="project-container bg-black">
-      <div className="p-contain-1">
-        <div className="links-wrap">
-          <a
-            href="https://github.com/Segmentation-Faulters-HackED-2025/fact-or-cap"
-            target="_blank"
-          >
-            <img
-              className="divider"
-              src="public\FactORCap.png"
-              alt="FactOrCap"
-            />
-          </a>
-          <center>
+    <section className="w-full bg-black py-1 px-6 relative">
+      <div className="flex flex-col items-center justify-center">
+        <h2 className="text-white font-black md:text-[60px] sm:text-[50px] xs:text-[40px] text-[30px]">
+          Projects
+        </h2>
+      </div>
+      <div className="project-container bg-black">
+        <div className="p-contain-1">
+          <div className="links-wrap space-x-4">
             <a
               href="https://github.com/Segmentation-Faulters-HackED-2025/fact-or-cap"
               target="_blank"
+              rel="noopener noreferrer"
             >
-              <button className="text-white">GitHub</button>
+              <img
+                className="w-80 h-auto object-cover rounded-lg border-4 border-white bg-white"
+                src="public\FactORCap.png"
+                alt="FactOrCap"
+              />
             </a>
-            <a
-              href="https://devpost.com/software/fact-or-cap-the-truth-checking-chrome-extention"
-              target="_blank"
-            >
-              <button className="text-white">DEVPOST</button>
-            </a>
-          </center>
+            <div className="text-center space-y-6 space-x-4">
+              <a
+                href="https://github.com/Segmentation-Faulters-HackED-2025/fact-or-cap"
+                target="_blank"
+              >
+                <button className="bg-blue-500 hover:bg-blue-700 text-white font-sans py-2 px-4 rounded-full">
+                  GitHub
+                </button>
+              </a>
+              <a
+                href="https://devpost.com/software/fact-or-cap-the-truth-checking-chrome-extention"
+                target="_blank"
+              >
+                <button className="bg-blue-500 hover:bg-blue-700 text-white font-sans py-2 px-4 rounded-full">
+                  DEVPOST
+                </button>
+              </a>
+            </div>
+          </div>
+        </div>
+        <div className="p-contain-2 text-white">
+          <h2 className="text-4xl font-extrabold dark:text-white">
+            Fact or Cap
+          </h2>
+          <p>
+            A Chrome extension that allows users to highlight any sentence on
+            the internet, right-click, and fact-check the selected text
+            instantly using verified sources.
+          </p>
+          <p>Computer Engineering Club HackED 2025</p>
+          <div className="icon-wrap items-center">
+            <button className="icon-house">
+              <p>JavaScript</p>
+            </button>
+            <button className="icon-house">
+              <p>HTML</p>
+            </button>
+            <button className="icon-house">
+              <p>CSS</p>
+            </button>
+            <button className="icon-house">
+              <p>Google Fact Checking API</p>
+            </button>
+          </div>
         </div>
       </div>
-      <div className="p-contain-2"></div>
-    </div>
+    </section>
   );
 };
 
