dd71dea30270371eec97896722b53776c6bcfb58
kkavinkk <gk.kavin05@gmail.com>
2025-03-24 13:30:48 -0600

    font for gallery

--- src/components/Featured_Gallery.tsx
+++ src/components/Featured_Gallery.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import "./gallery.css";
 
 const FeatureGallery: React.FC = () => {
   return (
@@ -15,18 +16,18 @@ const FeatureGallery: React.FC = () => {
       <div className="w-ful flex">
         <div className="mt-3 text-secondary text-[17px] leading-[30px] transform-none text-white">
           A curated collection of my smaller projects, creative experiments, and
-          cool things I’ve built along the way. From one-off ideas to quick
-          builds, this space highlights the fun, the functional, and everything
-          in between.s
+          cool things I’ve done along the way. From one-off ideas to quick
+          builds, or just stuff about me, this space highlights the fun, the
+          functional, and everything in between.
         </div>
       </div>
       <div className="mt-20 flex flex-wrap gap-7">
         {/* Gallery element 1 */}
         <div className="transform-none">
-          <div className="bg-[#261FB3] p-5 rounded-2x1 sm:w-[360px] w-full">
-            <div className="relative w-full h-[230px]">
+          <div className="bg-white p-5 rounded-2x1 sm:w-[360px] w-full">
+            <div className="relative w-full h-full">
               <img
-                src="src\assets\antony.jpg"
+                src="/Mybike.PNG"
                 alt="gal1"
                 className="w-full h-full object-cover rounded-2x1"
               />
@@ -34,10 +35,12 @@ const FeatureGallery: React.FC = () => {
             </div>
             <div className="mt-5">
               <div className="flex gap-2 items-center">
-                <h3 className="text-white font-bold text-[24px]">ANTONY</h3>
+                <h3 className="text-black font-bold font-[Shadows_Into_Light] text-[24px]">
+                  The worlds best bike
+                </h3>
               </div>
-              <p className="mt-2 text-secondary text-[14px] text-white">
-                ANTONY ANTONY ANTONY ANTONY
+              <p className="mt-2 font-bold font-[Shadows_Into_Light] text-[18px] text-black">
+                Always my favorite part of the summer
               </p>
             </div>
           </div>
